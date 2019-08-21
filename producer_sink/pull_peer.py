cd386e95474a4629a60116ecea059a6cdcdadd48
d2gex <daniel.garcia@d2garcia.com>
2019-08-21 10:29:13 +0100

    Refactor interface's signature to allow positional args work as expected

--- producer_sink/pull_peer.py
+++ producer_sink/pull_peer.py
@@ -12,11 +12,10 @@ class PullPeer(ipeer.IPeer):
     This class is not usually instantiated
     '''
 
-    def __init__(self, timeout=10, context=None, *args, **kwargs):
+    def __init__(self, *args, timeout=10, **kwargs):
         super().__init__(*args, **kwargs)
         self.timeout = timeout
         self.socket_type = zmq.PULL
-        self.context = context or zmq.Context()
         self.socket = self.context.socket(self.socket_type)
         self.socket.setsockopt(zmq.LINGER, self.linger)
         self.poller = zmq.Poller()
